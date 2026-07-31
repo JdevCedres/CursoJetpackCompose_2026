a4e8b027cc7af356ea078c65d7f987a52b1c5bff
jdev <jdevcedres@gmail.com>
2026-07-31 14:49:00 +0100

    Commit Switch, checkbox

--- app/src/main/java/com/jdev/myfirstcomposeapp/MainActivity.kt
+++ app/src/main/java/com/jdev/myfirstcomposeapp/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.jdev.myfirstcomposeapp.components.MyCheckbox
 import com.jdev.myfirstcomposeapp.components.MyIcon
+import com.jdev.myfirstcomposeapp.components.MySwitch
 import com.jdev.myfirstcomposeapp.components.practicas.ColoresPersonalizados
 import com.jdev.myfirstcomposeapp.components.practicas.IndicadorDeterminado
 import com.jdev.myfirstcomposeapp.components.practicas.MostarOcultar
@@ -26,7 +28,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyFirstComposeAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    ColoresPersonalizados (Modifier.padding(innerPadding))
+                    MyCheckbox (Modifier.padding(innerPadding))
 
                 }
             }
